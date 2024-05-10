2345c7724e95c8685c39a00deb8b70926dd5c02c
Alex Contreras <alexcontreras.alxlabs@gmail.com>
2024-05-09 18:40:20 -0600

    add O player and winner logic

--- src/App.js
+++ src/App.js
@@ -12,16 +12,36 @@ function Square({ value, onSquareClick }) {
 }
 
 export default function Board() {
+  const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
 
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = 'Winner: ' + winner;
+  } else {
+    status = 'Next player: ' + (xIsNext ? "X" : "O");
+  }
+
   function handleClick(idx) {
+    if (squares[idx] || calculateWinner(squares)) {
+      return;
+    }
+
     const nextSquares = squares.slice();
-    nextSquares[idx] = "X"
+    if (xIsNext) {
+      nextSquares[idx] = "X";
+    } else {
+      nextSquares[idx] = "O";
+    }
+
     setSquares(nextSquares);
+    setXIsNext(!xIsNext);
   }
 
   return (
     <>
+      <div className="status">{status}</div>
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
@@ -39,4 +59,24 @@ export default function Board() {
       </div>
     </>
   );
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
