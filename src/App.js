c836a5bdb40521b239186a9d98afbb4edcc33e79
Alex Contreras <alexcontreras.alxlabs@gmail.com>
2024-06-04 01:50:59 -0500

    use loops instead of hardcode Squares

--- src/App.js
+++ src/App.js
@@ -33,31 +33,31 @@ function Board({ xIsNext, squares, onPlay }) {
     status = 'Next player: ' + (xIsNext ? 'X' : 'O')
   }
 
+  const rows = [];
+  let idx = 0;
+  for (let i = 0; i < 3; i++) {
+    const cells = [];
+    for (let j = 0; j < 3; j++) {
+      const renderCell = (idx) => {
+        return <Square key={j} value={squares[idx]} onSquareClick={() => handleClick(idx)} />;
+      };
+      cells.push(renderCell(idx));
+      idx++;
+    }
+    rows.push(<div key={i} className="board-row">{cells}</div>);
+  }
+
   return (
     <>
       <div className="status">{status}</div>
-      <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-      </div>
-      <div>
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-      </div>
+      {rows}
     </>
   );
 }
 
 export default function Game() {
   const [history, setHistory] = useState([Array(9).fill(null)]);
-  const [currentMove, setCurrentMove]  = useState(0);
+  const [currentMove, setCurrentMove] = useState(0);
   const xIsNext = currentMove % 2 === 0;
   const currentSquares = history[currentMove];
 
